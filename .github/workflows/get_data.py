65327e1b35d0e56640c8ae0b1e8b05bbace0ddc1
Aonnghus <35961382+Aonnghus@users.noreply.github.com>
2021-12-03 18:33:12 +0100

    Added workflow

---
+++ .github/workflows/get_data.py
@@ -0,0 +1,38 @@
+import os
+
+from bs4 import BeautifulSoup
+from deta import Deta
+
+
+def main(db, file_path):
+    items = db.fetch(query=None, limit=1000)
+
+    with open(file_path, "r") as f:
+        soup = BeautifulSoup(f)
+
+    tbody = soup.body.find("table", {"id":"items-table"}).tbody
+    for item in items.items:
+        url = item["url"]
+        tr = soup.new_tag("tr")
+        td_cat = soup.new_tag("td")
+        td_url = soup.new_tag("td")
+        td_desc = soup.new_tag("td")
+        td_user = soup.new_tag("td")
+        td_cat.string = ", ".join(item["categories"])
+        td_url.string = item["url"]
+        td_desc.string = item["desc"]
+        td_user.string = item["user"]
+        tr.append(td_cat)
+        tr.append(td_url)
+        tr.append(td_desc)
+        tr.append(td_user)    
+        tbody.append(tr)
+
+    with open(file_path, "w") as f:
+        f.write(soup.prettify())
+
+if __name__ == "__main__":
+    deta = Deta(os.getenv("DETA_PROJECT_KEY"))
+    db = deta.Base("links")
+    file_path = "index.html"
+    main(db, file_path)
