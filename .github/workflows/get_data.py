319db9c158524d46f746cdf4f976219f79e11c67
Arnaud <5309511-aquillent@users.noreply.gitlab.com>
2021-12-05 16:10:19 +0100

    Updated workflow to stop appending already existing items

--- .github/workflows/get_data.py
+++ .github/workflows/get_data.py
@@ -1,35 +1,41 @@
 import os
 
 from bs4 import BeautifulSoup
+from more_itertools import last
 from deta import Deta
 
 
 def main(db, file_path):
-    items = db.fetch(query=None, limit=1000)
-
     with open(file_path, "r") as f:
         soup = BeautifulSoup(f)
 
-    tbody = soup.body.find("table", {"id":"items-table"}).tbody
-    for item in items.items:
-        url = item["url"]
-        tr = soup.new_tag("tr")
-        td_cat = soup.new_tag("td")
-        td_url = soup.new_tag("td")
-        td_desc = soup.new_tag("td")
-        td_user = soup.new_tag("td")
-        td_cat.string = ", ".join(item["categories"])
-        td_url.string = item["url"]
-        td_desc.string = item["desc"]
-        td_user.string = item["user"]
-        tr.append(td_cat)
-        tr.append(td_url)
-        tr.append(td_desc)
-        tr.append(td_user)    
-        tbody.append(tr)
+    table = soup.body.find("table", {"id":"items-table"})
+    last_key = table.attrs["data-last"]
+    items = db.fetch(limit=1000, last=last_key)
 
-    with open(file_path, "w") as f:
-        f.write(soup.prettify())
+    if items.count > 0:
+        tbody = table.tbody
+        for item in items.items:
+            tr = soup.new_tag("tr")
+            td_cat = soup.new_tag("td")
+            td_url = soup.new_tag("td")
+            td_desc = soup.new_tag("td")
+            td_user = soup.new_tag("td")
+            td_cat.string = ", ".join(item["categories"])
+            td_url.string = item["url"]
+            td_desc.string = item["desc"]
+            td_user.string = item["user"]
+            tr.append(td_cat)
+            tr.append(td_url)
+            tr.append(td_desc)
+            tr.append(td_user)    
+            tbody.append(tr)
+
+        last_key = items.items[-1]["key"]
+        table.attrs["data-last"] = last_key
+
+        with open(file_path, "w") as f:
+            f.write(soup.prettify())
 
 if __name__ == "__main__":
     deta = Deta(os.getenv("DETA_PROJECT_KEY"))
